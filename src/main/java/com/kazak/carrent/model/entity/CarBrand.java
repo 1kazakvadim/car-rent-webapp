24210f6791fdc5342c532d41cd76ea487f9c0fe2
1kazak.vadim <1kazak.vadim@gmail.com>
2021-06-13 13:46:15 +0300

    add CarBrand entity

---
+++ src/main/java/com/kazak/carrent/model/entity/CarBrand.java
@@ -0,0 +1,28 @@
+package com.kazak.carrent.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "car_brand")
+public class CarBrand {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false)
+  private Integer id;
+
+  @Column(name = "name", nullable = false, unique = true)
+  private String name;
+
+}
