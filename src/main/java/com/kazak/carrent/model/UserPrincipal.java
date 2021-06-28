d6d25cc5840e87b33d66f14fd32f690f12e34d2e
1kazakvadim <1kazakvadim@gmail.com>
2021-06-28 20:21:53 +0300

    add UserPrincipal

---
+++ src/main/java/com/kazak/carrent/model/UserPrincipal.java
@@ -0,0 +1,60 @@
+package com.kazak.carrent.model;
+
+import com.kazak.carrent.model.entity.User;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserPrincipal implements UserDetails {
+
+  private final User user;
+
+  public UserPrincipal(User user) {
+    this.user = user;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    if (null != user.getUserRole() && !user.getUserRole().getName().isEmpty()) {
+      return Arrays.stream(user.getUserRole().getName().split(","))
+          .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+          .collect(Collectors.toList());
+    }
+    return new ArrayList<>();
+  }
+
+  @Override
+  public String getPassword() {
+    return user.getPassword();
+  }
+
+  @Override
+  public String getUsername() {
+    return user.getUsername();
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return user.getStatus().equals("ACTIVE");
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+
+}
