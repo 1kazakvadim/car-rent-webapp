4e7a034c290e8416e341adf220279d0bba784411
1kazakvadim <1kazakvadim@gmail.com>
2021-06-28 22:32:52 +0300

    test

--- src/main/java/com/kazak/carrent/configuration/WebSecurityConfig.java
+++ src/main/java/com/kazak/carrent/configuration/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -31,24 +32,57 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     return new BCryptPasswordEncoder();
   }
 
-  protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+  public  void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
     auth.jdbcAuthentication().dataSource(dataSource);
   }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.
-        csrf().disable().authorizeRequests()
-//        .antMatchers("/user/new", "/user/**/edit").hasRole("admin")
-//        .antMatchers("/user", "/user/**/view").hasAnyRole("user", "admin")
-        .antMatchers("/login").permitAll()
+    http
+        .authorizeRequests()
+        .antMatchers("/resources/**").permitAll()
         .anyRequest().authenticated()
         .and()
         .formLogin()
-        .defaultSuccessUrl("/index")
+        .loginPage("/login")
+        .permitAll()
+        .defaultSuccessUrl("/")
         .and()
         .logout()
-        .deleteCookies("JSESSIONID");
+        .logoutSuccessUrl("/login")
+        .permitAll();
   }
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring()
+        .antMatchers(
+            "/css/**", "/fonts/**",
+            "/images/**");
+  }
+
+  //  @Override
+//  protected void configure(HttpSecurity http) throws Exception {
+//    http
+////        .csrf().disable()
+//        .authorizeRequests()
+//        .antMatchers("/resources/**").permitAll()
+////        .antMatchers("/user/new", "/user/**/edit").hasRole("admin")
+////        .antMatchers("/user", "/user/**/view").hasAnyRole("user", "admin")
+////        .antMatchers("/login").permitAll()
+//        .anyRequest().authenticated()
+//        .and()
+//        .formLogin()
+//        .loginPage("/login")
+//        .loginProcessingUrl("/perform-login")
+//        .usernameParameter("username")
+//        .passwordParameter("password")
+//        .permitAll()
+//        .defaultSuccessUrl("/index")
+//        .and()
+//        .logout()
+//        .permitAll();
+////        .deleteCookies("JSESSIONID");
+//  }
 
 }
