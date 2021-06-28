7f11f825a0c8c9b838c214f1bcc4cadd982eadd0
1kazakvadim <1kazakvadim@gmail.com>
2021-06-28 20:21:31 +0300

    add repo

---
+++ src/main/java/com/kazak/carrent/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.kazak.carrent.repository;
+
+import com.kazak.carrent.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+  User findByUsername(String username);
+
+}
