319d5a7a0a6713aee30111724842f4d6a48442a1
1kazak.vadim <1kazak.vadim@gmail.com>
2021-06-29 15:37:46 +0300

    add UserController

---
+++ src/main/java/com/kazak/carrent/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.kazak.carrent.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+  @GetMapping("/user")
+  public String userProfile(){
+    return "user";
+  }
+
+}
