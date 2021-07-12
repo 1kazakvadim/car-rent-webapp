3930205c333355a483698eb0af94b8dff1d99156
1kazakvadim <1kazakvadim@gmail.com>
2021-07-12 22:54:16 +0300

    fix

--- src/main/java/com/kazak/carrent/model/entity/CarOrder.java
+++ src/main/java/com/kazak/carrent/model/entity/CarOrder.java
@@ -3,42 +3,50 @@ package com.kazak.carrent.model.entity;
 import java.time.LocalDate;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "order")
-public class Order {
+@Table(name = "car_order")
+public class CarOrder {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "id", nullable = false)
   private Integer id;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "user_id", nullable = false)
   private User user;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "car_id", nullable = false)
+  @PrimaryKeyJoinColumn
   private Car car;
 
+  @DateTimeFormat(pattern = "yyyy-MM-dd")
   @Column(name = "date_of_issue", nullable = false)
   private LocalDate dateOfIssue;
 
+  @DateTimeFormat(pattern = "yyyy-MM-dd")
   @Column(name = "date_of_return", nullable = false)
   private LocalDate dateOfReturn;
 
+  @ColumnDefault("false")
   @Column(name = "cancellation", nullable = false)
   private boolean isCancellation;
 
