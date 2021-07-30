eec0b0f371b08f82e5a7648a49bf9ee8dd96616f
1kazakvadim <1kazakvadim@gmail.com>
2021-07-30 14:21:44 +0300

    fix

--- src/main/java/com/kazak/carrent/dto/PassportDataPostDto.java
+++ src/main/java/com/kazak/carrent/dto/PassportDataPostDto.java
@@ -11,7 +11,6 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 @Setter
-@ToString
 public class PassportDataPostDto {
 
   private Integer id;
--- src/main/java/com/kazak/carrent/dto/UserPostDto.java
+++ src/main/java/com/kazak/carrent/dto/UserPostDto.java
@@ -13,7 +13,6 @@ import lombok.ToString;
 
 @Getter
 @Setter
-@ToString
 public class UserPostDto {
 
   private Integer id;
