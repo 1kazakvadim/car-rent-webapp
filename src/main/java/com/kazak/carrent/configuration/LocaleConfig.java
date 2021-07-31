cf95a941d6bb43bde68428fd2a9a19eb27698307
1kazak.vadim <1kazak.vadim@gmail.com>
2021-07-31 17:09:45 +0300

    fix

--- src/main/java/com/kazak/carrent/configuration/LocaleConfig.java
+++ src/main/java/com/kazak/carrent/configuration/LocaleConfig.java
@@ -1,29 +1,19 @@
 package com.kazak.carrent.configuration;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Properties;
-import javax.annotation.Resource;
-import org.apache.logging.log4j.util.PropertiesPropertySource;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class LocaleConfig implements WebMvcConfigurer {
 
   @Bean
   public LocaleResolver localeResolver() {
-    CookieLocaleResolver  localeResolver = new CookieLocaleResolver ();
-    return localeResolver;
+    return new CookieLocaleResolver ();
   }
 
   @Bean
@@ -37,15 +27,6 @@ public class LocaleConfig implements WebMvcConfigurer {
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(localeChangeInterceptor());
   }
-
-//  @Bean
-//  public MessageSource getMessageResource() {
-//    ReloadableResourceBundleMessageSource messageResource = new ReloadableResourceBundleMessageSource();
-//    messageResource.setBasename("classpath:l10n");
-//    messageResource.setCacheSeconds(3600);
-//    messageResource.setDefaultEncoding("UTF-8");
-//    return messageResource;
-//  }
 
 }
 
