a2461092458755eac3989e6cf4e14a6f6c6c7091
1kazakvadim <1kazakvadim@gmail.com>
2021-08-02 21:46:10 +0300

    add RepairController

---
+++ src/main/java/com/kazak/carrent/controller/RepairController.java
@@ -0,0 +1,26 @@
+package com.kazak.carrent.controller;
+
+import com.kazak.carrent.model.entity.CarRepair;
+import com.kazak.carrent.service.CarRepairService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class RepairController {
+
+  private final CarRepairService carRepairService;
+
+  public RepairController(CarRepairService carRepairService) {
+    this.carRepairService = carRepairService;
+  }
+
+  @GetMapping("/profile/repair/{repairId}/damage-information")
+  public String getRepairDamageInformation(@PathVariable Integer repairId, Model model) {
+    CarRepair carRepair = carRepairService.findById(repairId);
+    model.addAttribute("carRepair", carRepair);
+    return "repair_damage_information";
+  }
+
+}
