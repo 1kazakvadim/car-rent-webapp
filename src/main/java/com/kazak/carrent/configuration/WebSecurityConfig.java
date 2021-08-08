59a08d40025008332830dd1184bc29786f606a64
1kazak.vadim <1kazak.vadim@gmail.com>
2021-08-08 16:38:38 +0300

    fix

--- src/main/java/com/kazak/carrent/configuration/WebSecurityConfig.java
+++ src/main/java/com/kazak/carrent/configuration/WebSecurityConfig.java
@@ -1,11 +1,9 @@
 package com.kazak.carrent.configuration;
 
-import com.kazak.carrent.service.impl.DatabaseUserDetailsServiceImpl;
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,7 +13,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
 class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   private final DataSource dataSource;
